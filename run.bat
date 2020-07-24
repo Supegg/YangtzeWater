999dfdaf5c3c496f81b1fae5c3772d913f027762
Supegg <supegg.rao@gmail.com>
2020-07-24 15:23:39 +0800

    more datas, auto commit

--- run.bat
+++ run.bat
@@ -1,9 +1,14 @@
-@echo off
+@echo on
 
 cd /d %~dp0
 call venv\\Scripts\\activate
 
 python water.py 2 >> run.log 2>&1
 
+git add .
+git commit -m "more datas, auto commit"
+git push
+
 deactivate
+
 exit
